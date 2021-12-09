32bd550fec5a7ee086c2575eea12191c33cf0f16
Daniel Zeng <dzeng@madmax4.stanford.edu>
2021-12-08 23:23:02 -0800

    data update

--- data.py
+++ data.py
@@ -38,13 +38,15 @@ def get_data(csv_file, feat_dim):
             movieIds[movieId] += len(userIds)
         csvfile.seek(0)
         next(csvreader)
-
+        
+        edge_set = set()
         edge_index_lst = []
         edge_attr_lst = []
         for row in csvreader:
             userId, movieId, rating = process_row(row)
             edge = [userIds[userId], movieIds[movieId]]
             if rating > THRESHOLD:
+                edge_set.add(tuple(edge))
                 edge_index_lst.append(edge)
                 edge_index_lst.append(edge[::-1])
         edge_index = torch.LongTensor(edge_index_lst).T
@@ -53,22 +55,33 @@ def get_data(csv_file, feat_dim):
         x = torch.ones(len(userIds) + len(movieIds), feat_dim)
         data = Data(x=x, edge_index=edge_index)
         
-        return data, len(userIds), len(movieIds), mp_mask, sup_mask, val_mask, test_mask
+        return data, len(userIds), len(movieIds), edge_set, mp_mask, sup_mask, val_mask, test_mask
 
 def get_data_cached(csv_file='ratings.csv', feat_dim=128, write_new_file=False):
     file_name = f"data_and_masks_{csv_file}_{feat_dim}.pt"
     if not write_new_file and os.path.exists(file_name):
         print(f"File exists, loading {file_name}")
         dct = torch.load(file_name)
-        return dct["data"], dct["num_users"], dct["num_movies"], dct["mp_mask"], \
-                dct["sup_mask"], dct["val_mask"], dct["test_mask"]
+        ret_keys = [
+            "data",
+            "num_users",
+            "num_movies",
+            "edge_set",
+            "mp_mask",
+            "sup_mask",
+            "val_mask",
+            "test_mask"
+        ]    
+        return [dct[k] for k in ret_keys]
+    
     else:
         print(f"File does not exist: {file_name}, creating")
-        data, num_users, num_movies, mp_mask, sup_mask, val_mask, test_mask = get_data(csv_file, feat_dim)
+        data, num_users, num_movies, edge_set, mp_mask, \
+            sup_mask, val_mask, test_mask = get_data(csv_file, feat_dim)
         torch.save(dict(data=data, mp_mask=mp_mask, sup_mask=sup_mask, 
                         val_mask=val_mask, test_mask=test_mask,
-                       num_users=num_users, num_movies=num_movies), file_name) 
-        return data, num_users, num_movies, mp_mask, sup_mask, val_mask, test_mask
+                       num_users=num_users, num_movies=num_movies, edge_set=edge_set), file_name) 
+        return data, num_users, num_movies, edge_set, mp_mask, sup_mask, val_mask, test_mask
 
 # def get_dataloader(args):
       # Doesnt work because this is for datasets which have multiple "data"s, where we just have one data
@@ -84,10 +97,10 @@ def main():
     # args = Args()
     # args.batch_size = 32
     # args.shuffle = True
-    data, num_users, num_movies, mp_mask, sup_mask, val_mask, test_mask = get_data_cached()
-    
-    pdb.set_trace()
+    data, num_users, num_movies, edge_set, mp_mask, sup_mask, val_mask, test_mask = get_data_cached()
     print(data) # Data(x=[10334], edge_index=[201672, 2], edge_attr=[201672])
+    pdb.set_trace()
+    
         
 if __name__ == "__main__":
     main()
