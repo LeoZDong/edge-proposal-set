ded274d15c5ee4088d1aa80f388cb9d7c8282ee6
Daniel Zeng <dzeng@madmax4.stanford.edu>
2021-12-08 23:17:59 -0800

    metrics.py

--- metrics.py
+++ metrics.py
@@ -1,32 +1,88 @@
 import torch
-
+from data import get_data_cached
 
 def top_k_edges(userEmbeds, movieEmbeds, k):
     dot_prod = userEmbeds @ movieEmbeds.T
     _, topK_indices = dot_prod.flatten().topk(k=k)
     numCols = movieEmbeds.shape[0]
-    rows = topK_indices // numCols
+    rows = torch.div(topK_indices, numCols, rounding_mode='floor')
     cols = topK_indices % numCols
     # sanity check with dot_prod[rows, cols]
     return rows, cols
 
-def recall(embeds, k):
-    pass
+def true_pos(edges_pred, gt_edges):
+    cnt = 0
+    for edge in edges_pred:
+        if tuple(edge) in gt_edges:
+            cnt += 1
+    return cnt
+
+def false_pos(edges_pred, gt_edges):
+    cnt = 0
+    for edge in edges_pred:
+        if tuple(edge) not in gt_edges:
+            cnt += 1
+    return cnt
+
+def false_neg(edges_pred, gt_edges):
+    gt_edges_cp = gt_edges.copy()
+    for edge in edges_pred:
+        if tuple(edge) in gt_edges_cp:
+            gt_edges_cp.remove(tuple(edge))
+    return len(gt_edges_cp)
+
+def metric_wrap(userEmbeds, movieEmbeds, k, gt_edges, metric):
+    rows, cols = top_k_edges(userEmbeds, movieEmbeds, k)
+    # currently seems to be faster if we did set lookup, unclear if there's a pytorch vectorized way
+    if metric == "recall":
+        return recall(zip(rows, cols), gt_edges)
+    elif metric == "precision":
+        return precision(zip(rows, cols), gt_edges)
+    elif metric == "precision_recall":
+        return precision_recall(zip(rows, cols), gt_edges)
+    return NotImplementedError
+
+def precision_recall(edges, gt_edges):
+    tp_count = true_pos(edges, gt_edges)
+    fn_count = false_neg(edges, gt_edges)
+    fp_count = false_pos(edges, gt_edges)
+    # print(tp_count, fn_count, fp_count)
+    try:
+        prec = tp_count / (tp_count + fp_count)
+    except ZeroDivisionError:
+        prec = None
+    try:
+        rec = tp_count / (tp_count + fn_count)
+    except ZeroDivisionError:
+        rec = None
+
+    return prec, rec 
+
+def recall(edges, gt_edges):
+    tp_count = true_pos(edges, gt_edges)
+    fn_count = false_neg(edges, gt_edges)
+    return tp_count / (tp_count + fn_count)
     
-def precision(embeds, k):
-    pass
+def precision(edges, gt_edges):
+    tp_count = true_pos(edges, gt_edges)
+    fp_count = false_pos(edges, gt_edges)
+    return tp_count / (tp_count + fp_count)
 
 import pdb
 def main():
     nU = 610
     nM = 9724
     dim = 128
-    k = 1000
+    print(f"{nU * nM} possible edges")
+    k = 1000000
     embeds = torch.rand((nU + nM, dim))
     userEmbeds = embeds[:nU]
     movieEmbeds = embeds[nU:]
     edges = top_k_edges(userEmbeds, movieEmbeds, k)
-    pdb.set_trace()
+    
+    data, num_users, num_movies, edge_set, mp_mask, sup_mask, val_mask, test_mask = get_data_cached()
+    precision, recall = metric_wrap(userEmbeds, movieEmbeds, k, edge_set, "precision_recall")
+    print(precision, recall)
         
 if __name__ == "__main__":
     main()
